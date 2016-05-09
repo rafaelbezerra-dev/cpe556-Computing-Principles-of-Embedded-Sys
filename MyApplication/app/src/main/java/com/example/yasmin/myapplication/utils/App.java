570b3a4b7a95570350604a46bd0c998fb5a3b18f
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-05-09 19:07:43 -0400

    android app

--- MyApplication/app/src/main/java/com/example/yasmin/myapplication/utils/App.java
+++ MyApplication/app/src/main/java/com/example/yasmin/myapplication/utils/App.java
@@ -4,8 +4,8 @@ package com.example.yasmin.myapplication.utils;
  * Created by Rafael on 5/9/2016.
  */
 public class App {
-//    public static final String DEFAULT_HOST_URI = "http://155.246.76.25:8000";
-    public static final String DEFAULT_HOST_URI = "http://10.0.2.2:5000";
+    public static final String DEFAULT_HOST_URI = "http://155.246.76.25:8000";
+//    public static final String DEFAULT_HOST_URI = "http://10.0.2.2:5000";
     public static final String DEFAULT_CONTENT_TYPE = "application/json";
     public static final String DEFAULT_ENCODING = "UTF-8";
 
